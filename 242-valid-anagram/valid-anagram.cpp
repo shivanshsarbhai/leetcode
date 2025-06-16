7b5a8598325e60814f59cb9ce0a0a4c41ac4649e
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2025-06-16 09:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     bool brute(string s, string t){
+        //TC : O(NlogN + MlogM)
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
         return s == t;
     }
 
+    bool better(string s, string t){
+        if(s.length() != t.length())
+            return false;
+
+        vector<int>freq(26,0);
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0)
+                return false;
+        }
+
+        return true;
+    }
+
     bool isAnagram(string s, string t) {
-        return brute(s, t);
+        // return brute(s, t);
+        return better(s,t);
     }
 };
