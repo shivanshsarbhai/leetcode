26967dc0807387dc782cb319d88142ffd90895d4
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2025-08-05 16:38:57 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (16.69%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int globalMax = nums[0];
+        int localMax = nums[0];
+        for(int i=1;i<nums.size();i++){
+            localMax = max(nums[i], localMax+nums[i]);
+            globalMax = max(globalMax, localMax);
+        }
+
+        return globalMax;
+    }
+};
