a92296a34f889b0985af5aa61cf45d8820d53dfe
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2025-08-21 20:45:15 +0530

    Time: 2 ms (71.33%) | Memory: 9.2 MB (92.40%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -15,15 +15,14 @@ public:
     }
 
     string multiply(string nums1, string nums2) {
-        equalize(nums1, nums2);
         int n = nums1.length();
         reverse(nums1.begin(), nums1.end());
         reverse(nums2.begin(), nums2.end());
         vector<int>ans(400,0);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums1.length();i++){
             int carry = 0;
-            for(int j=0;j<n;j++){
-                int index = i+j;
+            for(int j=0;j<nums2.length();j++){
+                int index = i+j; //important
                 int d1 = nums1[i]-'0';
                 int d2 = nums2[j]-'0';
                 int mulp = (ans[index] + d1*d2 + carry);
@@ -32,7 +31,7 @@ public:
             }
 
             if(carry > 0)
-                ans[i+n] = carry; 
+                ans[i+nums2.length()] += carry; 
         }
 
         int i = ans.size() - 1;
