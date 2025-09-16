ba475ed435946c632a66466ab93eb2e8b94afe17
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2025-09-16 08:17:28 +0530

    Time: 3 ms (32.20%) | Memory: 20.3 MB (84.80%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -7,9 +7,8 @@ public:
             dp[i] = min(dp[i], val);
         }
     }
-
-    int jump(vector<int>& nums) {
-        int n = nums.size();
+    int brute(vector<int>&nums){
+       int n = nums.size();
         vector<int>dp(n,INT_MAX);
         dp[0] = 0;
 
@@ -20,13 +19,35 @@ public:
 
         return dp[n-1];
     }
+    int optimal(vector<int>&nums){
+        int n = nums.size();
+        int jumps = 0;
+        int maxReach = 0;
+        int endIndex = 0;
+
+        for(int i=0;i<n-1;i++){
+            maxReach = max(maxReach, i+nums[i]);
+            if(i==endIndex){
+                jumps++;
+                endIndex = maxReach;
+            }
+        }
+
+        return jumps;
+    }
+
+    int jump(vector<int>& nums) {
+        return optimal(nums);
+    }
 };
 
 /*
-[1,2,3]
-[0,X,X]
-[0,1,X]
+[2,3,1,1,4]
+       ^
 
-
+i=4
+maxReach = 4;
+jumps = 2;
+endIndex = 4;
 
 */
