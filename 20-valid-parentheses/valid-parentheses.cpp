73a316612caee884e6ee6e38bc135a19d489e28d
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-06-19 01:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     bool isValid(string str) {
+        map<char, char>mp;
+        mp.insert({')', '('});
+        mp.insert({']', '['});
+        mp.insert({'}', '{'});
+
         stack<char>s;
-        map<char,char>mp;
-        mp[']'] = '[';
-        mp['}'] = '{';
-        mp[')'] = '(';
-
         for(int i=0;i<str.length();i++){
-            if(s.empty()){
-                s.push(str[i]);
-                continue;
-            } 
-
-            if(str[i] == ')' || str[i]=='}' || str[i]==']')
-            {
-                if(s.top()==mp[str[i]]){
-                    s.pop();    
-                    continue;
-                }
-            }
-
-            s.push(str[i]);
+            char ele = str[i];
+            if(!s.empty() && mp.find(ele) != mp.end() && s.top() == mp[ele])
+                s.pop();
+            else
+                s.push(ele);
         }
 
         return s.empty();
