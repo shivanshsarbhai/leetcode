8748c6f466a48d9be4ce26fe04c34eea644c05fe
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-06-24 18:17:59 +0530

    Time: 2 ms (92.56%) | Memory: 23.9 MB (31.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<vector<int>>ans;
-        ans.push_back(nums[0]);
-        int index = 0;
-        for(int i=1;i<n;i++){
-            int prevStart = ans[index][0];
-            int prevEnd = ans[index][1];
-            int start = nums[i][0];
-            int end = nums[i][1];
+        sort(nums.begin(), nums.end());
 
-            if(start > prevEnd){
+        for(int i=0;i<n-1;i++){
+            int firstEnd = nums[i][1];
+            int secondStart = nums[i+1][0];
+            int firstStart = nums[i][0];
+            int secondEnd = nums[i+1][1];
+            if(firstEnd < secondStart)
                 ans.push_back(nums[i]);
-                index++;
-            }
-            else{
-                ans[index][0] = min(ans[index][0], start);
-                ans[index][1] = max(ans[index][1], end);
-            }
+            else
+                nums[i+1] = {firstStart, max(firstEnd, secondEnd)}; 
         }
 
-        return ans;
+        ans.push_back(nums[n-1]);
+        return ans; 
     }
 };
