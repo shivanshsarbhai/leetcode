8256a322af204e6b069449694b71f2e2b31e7c02
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-06-28 21:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.61%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int maxSoFar = nums[0];
         int globalMax = nums[0];
-        int localMax = nums[0];
+
         for(int i=1;i<nums.size();i++){
-            localMax = max(nums[i], localMax+nums[i]);
-            globalMax = max(globalMax, localMax);
+            maxSoFar = max(nums[i], maxSoFar + nums[i]);
+            globalMax = max(globalMax, maxSoFar);
         }
 
         return globalMax;
