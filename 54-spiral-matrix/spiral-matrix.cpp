8957576aba3ac802bb6d098d98a957f2125e67b2
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-07-01 07:35:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (15.57%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -4,25 +4,29 @@ public:
         int n = nums.size();
         int m = nums[0].size();
         int top = 0;
+        int bottom = n - 1;
         int left = 0;
         int right = m-1;
-        int bottom = n-1;
+
         vector<int>ans;
-        while(top <= bottom && left <= right){
+        while(top<=bottom && left<=right){
             
-            for(int i=left;i<=right;i++)
-                ans.push_back(nums[top][i]);
+            for(int j=left;j<=right;j++)
+                ans.push_back(nums[top][j]);
             top++;
 
             for(int i=top;i<=bottom;i++)
                 ans.push_back(nums[i][right]);
             right--;
 
-            if(top<=bottom && left<=right){
-                for(int i=right;i>=left;i--)
-                    ans.push_back(nums[bottom][i]);
+            if(top<=bottom){
+                for(int j=right;j>=left;j--)
+                    ans.push_back(nums[bottom][j]);
                 bottom--;
+            }
 
+            
+            if(right>=left){
                 for(int i=bottom;i>=top;i--)
                     ans.push_back(nums[i][left]);
                 left++;
