cbbee816f3e435b08c4b944075d1559edccebc1d
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-07-03 09:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (93.84%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& nums) {
+    void better(vector<vector<int>>& nums){
+        // TC : O(n*m) ; SC : O(n) + O(m);
         int n = nums.size();
         int m = nums[0].size();
 
@@ -21,5 +22,62 @@ public:
                 nums[i][j]*=col[j]*row[i];
             }
         }
+    }
+
+    void optimal(vector<vector<int>>&nums){
+        bool setFirstRowZero = false;
+        bool setFirstColZero = false;
+        int n = nums.size();
+        int m = nums[0].size();
+
+        for(int i=0;i<n;i++){
+            if(nums[i][0]==0){
+                setFirstColZero = true;
+                break;
+            }
+        }
+
+        for(int j=0;j<m;j++){
+            if(nums[0][j]==0){
+                setFirstRowZero=true;
+                break;
+            }
+        }
+
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(nums[i][j]==0){
+                    nums[i][0] = 0;
+                    nums[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(nums[i][0]==0 || nums[0][j]==0)
+                    nums[i][j]=0;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(setFirstColZero){
+                nums[i][0]=0;
+            }
+        }
+
+        for(int j=0;j<m;j++){
+            if(setFirstRowZero){
+                nums[0][j]=0;
+            }
+        }
+
+    }
+
+    void setZeroes(vector<vector<int>>& nums) {
+        // better(nums);
+        optimal(nums);
     }
 };
