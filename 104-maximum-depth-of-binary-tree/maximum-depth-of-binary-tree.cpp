d1f456549c06ca861d60bca46d1aa7363d64c3ff
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-08-14 15:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (61.63%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,9 +15,6 @@ public:
         if(!root)
             return 0;
         
-        if(!root->left && !root->right)
-            return 1;
-        
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
