a0b0c8eb38de6b8e1176ecf7b937fa7ae6a78a49
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-08-16 18:43:27 +0530

    Time: 52 ms (45.74%) | Memory: 93.8 MB (91.29%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    static bool comp(vector<int>&a, vector<int>&b){
-        return a[1]<b[1];
+    static bool sortByEndTime(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
     }
 
     int eraseOverlapIntervals(vector<vector<int>>& nums) {
-        sort(nums.begin(), nums.end(), comp);
+        sort(nums.begin(), nums.end(), sortByEndTime);
+
         int ans = 0;
-        int lastNonOverLapping = 0;
+        int prevEnd = nums[0][1];
 
-        for(int i=1;i<nums.size();i++){
-            int prevStart = nums[lastNonOverLapping][0];
-            int prevEnd = nums[lastNonOverLapping][1];
-            int start = nums[i][0];
-            int end = nums[i][1];
-
-            if(start>=prevEnd)
-            {
-                lastNonOverLapping = i;
-                continue;
-            }
-            
-            else
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i][0] < prevEnd) {
+                // Overlap → remove current
                 ans++;
+            } else {
+                // No overlap → keep current
+                prevEnd = nums[i][1];
+            }
         }
 
         return ans;
     }
 };
-
-/*
-
-(1,11) (2, 12) (11,22) (1 100)
-
-*/
