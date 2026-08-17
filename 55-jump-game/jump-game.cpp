b2943055ea15795e3d41149768e57de193badb45
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-08-17 20:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (83.39%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,36 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-
-        if(n==1)
-            return true;
-
-        int maxFromHere = nums[0];
-        int maxFromBefore = nums[0];
-        for(int i=0;i<n;i++){
-            if(i<=maxFromBefore){
-                maxFromHere = i+nums[i];
-                maxFromBefore = max(maxFromBefore , maxFromHere);
-            }
+        int maxCanReach = nums[0];
+        for(int i=1;i<n;i++){
+            if(maxCanReach >= i)
+                maxCanReach = max(maxCanReach, i+nums[i]);
         }
 
-        return maxFromBefore >= n-1;
+        return maxCanReach >= n-1;
     }
 };
-
-/*
-maxFromBefore = 2
-maxFromHere   = 2
-
-i=1
-maxFromHere = 3
-maxFromBefore = 3
-
-i=2
-maxFromHere = 3
-maxFromBefore = 3
-
-i=3
-maxFromHere = 3
-maxFromBefore = 3
-*/
