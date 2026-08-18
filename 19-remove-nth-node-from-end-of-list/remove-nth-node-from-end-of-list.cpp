bbfa857fd3b57615db4624d12759bb37a7884eff
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-08-18 20:29:56 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (26.97%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -16,15 +16,18 @@ public:
         ListNode* slow = prehead;
         ListNode* fast = prehead;
 
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++){
             fast = fast->next;
-        
+        }
+
         while(fast->next){
             slow = slow->next;
             fast = fast->next;
         }
 
-        slow->next = (slow->next)->next;
+        if(slow->next)
+            slow->next = (slow->next)->next;
+        
         return prehead->next;
     }
 };
