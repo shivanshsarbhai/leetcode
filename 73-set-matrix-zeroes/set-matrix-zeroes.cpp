26da30f73daa24761a193e6ef58bd1569d980f65
Shivansh Sarbhai <shivanshsarbhai0607@gmail.com>
2026-08-20 17:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (40.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,84 +1,82 @@
 class Solution {
 public:
-    void better(vector<vector<int>>& nums){
-        // TC : O(n*m) ; SC : O(n) + O(m);
+    void suboptimal(vector<vector<int>>&nums) {
+        //TC : O(nm)
+        //SC : O(n + m)
         int n = nums.size();
         int m = nums[0].size();
-
-        vector<int>row(n, 1);
-        vector<int>col(m, 1);
+        vector<int>rows(n, 1);
+        vector<int>cols(m, 1);
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(nums[i][j]==0){
-                    row[i]=0;
-                    col[j]=0;
+                if(nums[i][j] == 0){
+                    rows[i] = 0;
+                    cols[j] = 0;
                 }
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                nums[i][j]*=col[j]*row[i];
+                if(rows[i]==0 || cols[j]==0)
+                    nums[i][j] = 0;
             }
         }
     }
 
-    void optimal(vector<vector<int>>&nums){
-        // TC : O(n*m) ; SC : O(1);
-        bool setFirstRowZero = false;
-        bool setFirstColZero = false;
-        int n = nums.size();
-        int m = nums[0].size();
+void optimal(vector<vector<int>>& nums) {
+    int n = nums.size();
+    int m = nums[0].size();
 
-        for(int i=0;i<n;i++){
-            if(nums[i][0]==0){
-                setFirstColZero = true;
-                break;
-            }
-        }
+    bool firstRow = false;
+    bool firstCol = false;
 
-        for(int j=0;j<m;j++){
-            if(nums[0][j]==0){
-                setFirstRowZero=true;
-                break;
-            }
-        }
-
-
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(nums[i][j]==0){
-                    nums[i][0] = 0;
-                    nums[0][j] = 0;
-                }
-            }
-        }
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(nums[i][0]==0 || nums[0][j]==0)
-                    nums[i][j]=0;
-            }
-        }
-
-        for(int i=0;i<n;i++){
-            if(setFirstColZero){
-                nums[i][0]=0;
-            }
-        }
-
-        for(int j=0;j<m;j++){
-            if(setFirstRowZero){
-                nums[0][j]=0;
-            }
-        }
-
+    // Check first row
+    for (int j = 0; j < m; j++) {
+        if (nums[0][j] == 0)
+            firstRow = true;
     }
+
+    // Check first column
+    for (int i = 0; i < n; i++) {
+        if (nums[i][0] == 0)
+            firstCol = true;
+    }
+
+    // Use first row/column as markers
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (nums[i][j] == 0) {
+                nums[i][0] = 0;
+                nums[0][j] = 0;
+            }
+        }
+    }
+
+    // Zero marked rows/columns
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (nums[i][0] == 0 || nums[0][j] == 0)
+                nums[i][j] = 0;
+        }
+    }
+
+    // Zero first row
+    if (firstRow) {
+        for (int j = 0; j < m; j++)
+            nums[0][j] = 0;
+    }
+
+    // Zero first column
+    if (firstCol) {
+        for (int i = 0; i < n; i++)
+            nums[i][0] = 0;
+    }
+}
 
     void setZeroes(vector<vector<int>>& nums) {
-        // better(nums);
+        // suboptimal(nums);
         optimal(nums);
     }
 };
